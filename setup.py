956c80647d4876102a51a99fee4e5844459314ef
Sasha Rush <srush.researech@gmail.com>
2022-07-17 11:53:05 -0400

    remove c package

--- setup.py
+++ setup.py
@@ -7,15 +7,6 @@ import sys
 import shutil
 from distutils.core import setup, Extension
 
-try:
-    from distutils.command.build_py import build_py_2to3 as build_py
-except ImportError:
-    if sys.version_info >= (3, 0):
-        raise ImportError("build_py_2to3 not found in distutils - it is required for Python 3.x")
-    from distutils.command.build_py import build_py
-    suffix = ""
-else:
-    suffix = "-py3k"
 
 srcdir = os.path.dirname(__file__)
 
@@ -24,12 +15,6 @@ def read(fname):
 
 include_dirs = ['include']
 extra_compile_args = []
-
-if 'SETUP_PY_CFLAGS' in os.environ:
-	# SETUP_PY_CFLAGS allows you to pass in CFLAGS
-	# in a disutils-friendly way. Using CFLAGS directly
-	# causes linking to fail for some python versions
-	extra_compile_args.append(os.environ['SETUP_PY_CFLAGS'])
 
 setup(
     name='planar',
@@ -59,24 +44,6 @@ setup(
 
     package_dir={'planar': 'lib/planar',
                  'planar.test': 'test'},
-    packages=['planar', 'planar.test'], 
-	ext_modules=[
-		Extension('planar.c', 
-			['lib/planar/cmodule.c', 
-			 'lib/planar/cvector.c',
-			 'lib/planar/ctransform.c',
-			 'lib/planar/cline.c',
-			 'lib/planar/cbox.c',
-			 'lib/planar/cpolygon.c',
-			], 
-			include_dirs=include_dirs,
-			#library_dirs=library_dirs,
-			#libraries=libraries,
-			#extra_link_args=extra_link_args,
-			extra_compile_args=extra_compile_args,
-			#define_macros=macros,
-		),
-    ],
+    packages=['planar', 'planar.test'],
 
-    cmdclass = {'build_py': build_py},
 )
