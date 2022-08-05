69ee1460e7bf205b23530e2ede5c4aa9bb300463
Sasha Rush <srush.research@gmail.com>
2022-08-05 18:24:35 -0400

    typing

---
+++ planar/__init__.pyi
@@ -0,0 +1,10 @@
+from planar.box import BoundingBox as BoundingBox
+from planar.line import Line as Line, LineSegment as LineSegment, Ray as Ray
+from planar.polygon import Polygon as Polygon
+from planar.transform import Affine as Affine
+from planar.vector import Seq2 as Seq2, Vec2 as Vec2, Vec2Array as Vec2Array
+
+class TransformNotInvertibleError(Exception): ...
+Point = Vec2
+
+def set_epsilon(epsilon) -> None: ...
