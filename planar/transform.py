a50c4ec04d68b6c0745b44308d31b4cd4fc68c4b
Sasha Rush <srush.research@gmail.com>
2022-08-05 22:12:23 -0400

    typing

--- planar/transform.py
+++ planar/transform.py
@@ -25,7 +25,7 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 # EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #############################################################################
-
+from __future__ import annotations
 
 import math
 import planar
--- planar/vector.py
+++ planar/vector.py
@@ -1,4 +1,4 @@
-from __future__ import annotations
+
 #############################################################################
 # Copyright (c) 2010 by Casey Duncan
 # Portions copyright (c) 2009 The Super Effective Team 
@@ -28,7 +28,7 @@ from __future__ import annotations
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 # EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #############################################################################
-
+from __future__ import annotations
 
 import math
 import planar
